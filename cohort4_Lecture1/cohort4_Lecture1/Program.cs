72a4a5b3505a1f3fd5c053f9bb777b19ee6de632
batool aldalki  -gt <gt.batool.aldalki@Jt.JtGroup>
2024-06-25 21:27:16 +0300

    test

---
+++ cohort4_Lecture1/cohort4_Lecture1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace cohort4_Lecture1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arrayOfData = { 1, 2, 3, 4, 5 };
+
+            //arrayOfData[4] = 10;
+            //Console.WriteLine(arrayOfData);
+
+
+            //int[] arrayOfData = new int[] { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine(arrayOfData[4]);
+
+
+            var arrayOfData = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(arrayOfData[4]);
+
+
+
+        }
+    }
+}
